805fc78b9d56b7f34567357a8a2a51c1eec43676
Lee HoJin <40799363+roycmlhj@users.noreply.github.com>
2022-11-06 20:01:56 +0900

    Add files via upload

--- ThisIsCodingTest/Chapter8/일로만들기.java
+++ ThisIsCodingTest/Chapter8/일로만들기.java
@@ -8,7 +8,22 @@ public class 일로만들기 {
 		Scanner sc = new Scanner(System.in);
 
         int x = sc.nextInt();
-
+        
+        for (int i = 2; i <= x; i++) {
+            // 현재의 수에서 1을 빼는 경우
+            arr[i] = arr[i - 1] + 1;
+            // 현재의 수가 2로 나누어 떨어지는 경우
+            if (i % 2 == 0)
+                arr[i] = Math.min(arr[i], arr[i / 2] + 1);
+            // 현재의 수가 3으로 나누어 떨어지는 경우
+            if (i % 3 == 0)
+                arr[i] = Math.min(arr[i], arr[i / 3] + 1);
+            // 현재의 수가 5로 나누어 떨어지는 경우
+            if (i % 5 == 0)
+                arr[i] = Math.min(arr[i], arr[i / 5] + 1);
+        }
+        
+        System.out.println(arr[x]);
 	}
 
 }
