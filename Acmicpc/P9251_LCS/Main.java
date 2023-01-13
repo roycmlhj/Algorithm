59c931a0339415311f87cf3ab7a2d5c3ffff03a4
Lee HoJin <40799363+roycmlhj@users.noreply.github.com>
2023-01-13 16:38:20 +0900

    P9251_solved

--- Acmicpc/P9251_LCS/Main.java
+++ Acmicpc/P9251_LCS/Main.java
@@ -1,11 +1,9 @@
-package dp;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_9251 {
+public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
