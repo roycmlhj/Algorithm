52167a3136d14269f2e3a4dbae766ca436c4707e
Lee HoJin <40799363+roycmlhj@users.noreply.github.com>
2023-01-25 20:42:24 +0900

    Create Solution.java

--- Programmers/Level2/구명보트/Solution.java
+++ Programmers/Level2/구명보트/Solution.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
-        int answer = 0;
+        int answer = 0; 
 		Arrays.sort(people);
 		int min_index = 0;
 		int max_index = people.length - 1;
