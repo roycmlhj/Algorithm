b007bdce8ec98f577e01728c7e518f1ab68a97bb
Lee HoJin <40799363+roycmlhj@users.noreply.github.com>
2023-02-06 18:19:22 +0900

    Create Solution.java

---
+++ Programmers/Level3/순위/Solution.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int solution(int n, int[][] results) {
+        int answer = 0;
+        int[][] map = new int[n+1][n+1];
+        
+        for(int i = 0; i < results.length; i++){
+            map[results[i][0]][results[i][1]] = 1; 
+            map[results[i][1]][results[i][0]] = -1; 
+        }
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                for(int k = 1; k <= n; k++){
+                    if(map[i][k] == 1 && map[k][j] == 1){
+                        map[i][j] = 1;
+                        map[j][i] = -1;
+                    }
+                    if(map[i][k] == -1 && map[k][j] == -1){
+                        map[i][j] = -1;
+                        map[j][i] = 1;
+                    }
+                }
+            }
+        }
+        
+        for(int i = 1; i <= n; i++){
+            int cnt = 0; 
+            for(int j = 1; j <= n; j++){
+                if(map[i][j] != 0){
+                    cnt++;
+                }
+            }
+            if(cnt == n-1){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
