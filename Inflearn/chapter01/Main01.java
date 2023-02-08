8567f73b81e7b3546d656ca97c58c48fe9db234c
Lee HoJin <40799363+roycmlhj@users.noreply.github.com>
2023-02-08 17:49:39 +0900

    Main01_solved

--- Inflearn/chapter01/Main01.java
+++ Inflearn/chapter01/Main01.java
@@ -8,7 +8,7 @@ public class Main01 {
 	/*
 	 * 한 개의 문자열을 입력받고,
 	 * 특정 문자를 입력받아 해당 특정문자가
-	 * 입력받은 문자열에 몇 개 존재하는지 알아내는 프로그램을 작성*/
+	 * 입력받은 문자열에 몇 개 존재하는지 알아내는 프로그램을 작성*/ 
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
